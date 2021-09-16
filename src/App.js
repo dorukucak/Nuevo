4c6f72a96aa8ec1906fdc151ef8269a82c3d284f
Doruk <dorukucak@live.com>
2021-09-16 12:09:32 +0300

    pagination done

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import Header from './components/header';
 import Filter from './components/filter';
 import List from './components/list';
 import Search from './components/search';
+import Pagination from './components/pagination';
 
 
 
 const App = () => {
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
   const [show, setShow] = useState(null);
   const [searched, setSearched] = useState(""); 
+  // Pagination
+  const [cardsPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+  //Get current number of individuals
+  const indexOfLastCard = currentPage * cardsPerPage ;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = data.slice(indexOfFirstCard, indexOfLastCard);
+  const paginateFront = () => setCurrentPage(currentPage + 1);
+  const paginateBack = () => setCurrentPage(currentPage - 1);
 
   const handleSearchClick = (e) => {
     e.preventDefault();
     setShow(true);
     setSearched(search);    // prevents immediate search change - sends what search value we want to <List />
   }
-
 
   useEffect(() => {
     fetch("https://5f7335deb63868001615f557.mockapi.io/list")
@@ -47,19 +56,25 @@ const App = () => {
       <div id="main container" className="px-20">
         <Header />
         <div className="grid grid-cols-3 justify-center">
-        <div className={(show) ? "block" : "hidden"}>
+        <div className={(show) ? "block h-full" : "hidden"}>
           <Filter />
           </div>
           <div className="col-span-2 flex flex-col space-y-10  ">
             <Search data={data} onChange={(e) => setSearch(e.target.value)} onSubmit={handleSearchClick}/>
             <div className={(show) ? "block" : "hidden"}>
-            <List data={data} search={searched} loading={loading} error={error}/>
+            <List data={currentCards} search={searched} loading={loading} error={error}/>
             </div>
           </div>
         </div>
-
-
+        <Pagination
+        postsPerPage={cardsPerPage}
+        totalPosts={data.length}
+        paginateBack={paginateBack}
+        paginateFront={paginateFront}
+        currentPage={currentPage}
+      />
       </div>
+     
     </>
   );
 }
